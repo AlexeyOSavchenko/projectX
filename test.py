5587c7693e7abd52c1a3335a40efc077e597386f
Alex <alexeyosavchenko@gmail.com>
2022-01-26 12:18:40 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print("test")
+import requests
+
+client_id = '0feca865-844a-4260-939a-a725430e72cc'
+
+endpoint = 'https://frost.met.no/observations/v0.jsonld'
+parameters = {
+    'sources': 'SN18700,SN90450',
+    'elements': 'mean(air_temperature P1D),sum(precipitation_amount P1D),mean(wind_speed P1D)',
+    'referencetime': '2010-04-01/2010-04-03',
+}
+# Issue an HTTP GET request
+r = requests.get(endpoint, parameters, auth=(client_id,''))
+# Extract JSON data
+json = r.json()
+
+if r.status_code == 200:
+    data = json['data']
+    print('Data retrieved from frost.met.no!')
+else:
+    print('Error! Returned status code %s' % r.status_code)
+    print('Message: %s' % json['error']['message'])
+    print('Reason: %s' % json['error']['reason'])
+
+    
