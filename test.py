35610e8283b1cc9f96f74ca3d94789c0acf0a029
AlexeyOSavchenko <AlexeyOSavchenko@users.noreply.github.com>
2022-02-18 10:29:45 +0500

    Update get data from frost.met.no

--- test.py
+++ test.py
@@ -4,19 +4,33 @@ import json
 client_id = '0feca865-844a-4260-939a-a725430e72cc'
 
 sources_endpoint = 'https://frost.met.no/sources/v0.jsonld'
-sources_parameters = {'ids': 'SN*', 'country': 'NO'}
+sources_parameters = {'ids': 'SN*', 'country': 'NO', 'elements': 'wind_from_direction'}
 
-observations_endpoint = 'https://frost.met.no/observations/v0.jsonld'
-observations_parameters = {'sources': 'SN18700,SN90450', 'elements': 'mean(wind_speed P1D)', 'referencetime': '2010-04-01/2020-04-03'}
 
 def get_data(endpoint, parameters):
-    responce = requests.get(endpoint, parameters, auth=(client_id, ''))
-    if responce.status_code == 200:
-        print('Data retrieved from frost.met.no!')
-    else:
-        print('Error! Returned status code %s' % responce.status_code)
-    json_data = responce.json()
+    response = requests.get(endpoint, parameters, auth=(client_id, '')).json()
+    json_data = response['data']
     return json_data
 
-print(get_data(sources_endpoint, sources_parameters))
-print(get_data(observations_endpoint, observations_parameters))
+
+sources_data = get_data(sources_endpoint, sources_parameters)
+
+list_date = []
+for key in range(len(sources_data)):
+    list_date.append(sources_data[key]["validFrom"])
+list_date.sort()
+list_oldest10 = list_date[0:10]
+
+list_sources_id = []
+for key in range(len(sources_data)):
+    if sources_data[key]["validFrom"] in list_oldest10:
+        list_sources_id.append(sources_data[key]["id"])
+
+string_src_id = ','
+observations_endpoint = 'https://frost.met.no/observations/v0.jsonld'
+observations_parameters = {'sources': string_src_id.join(list_sources_id), 'elements': 'mean(wind_from_direction PT1H)',
+                           'referencetime': '2010-04-01/2020-04-03'}
+
+observations_data = get_data(observations_endpoint, observations_parameters)
+print(observations_data)
+
