5d86d94e7489d88faf4f3d7e944718cf4327f64d
AlexeyOSavchenko <AlexeyOSavchenko@users.noreply.github.com>
2022-05-03 18:09:24 +0500

    Update tests.py

--- projectXback/meteo/tests.py
+++ projectXback/meteo/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Stations
 
-# Create your tests here.
+class ViewsTestCase(TestCase):
+    def test_api_stations_load_properly(self):
+        response = self.client.get('http://127.0.0.1:8000/api/stations')
+        self.assertEqual(response.status_code, 200)
+
+class ModelsTestCase(TestCase):
+    def test_insert_to_DB(self):
+        tst_st = Stations.objects.create(ids='test_id', name='test_name', country='Russia', coordinateX='0.0', coordinateY='0.0', validFrom='1970-01-01')
+
